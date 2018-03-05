929627040cb013bd7a9d6348df5c997751f8f13a
WillForan <willforan@gmail.com>
2018-03-05 16:55:13 -0500

    add start of qualtircs pulling code

---
+++ behave/000_getQualtrics.R
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+
+install.packages('qualtRics'); library('qualtRics')
+
+registerOptions(api_token='76qsEM2voOECkoYZHDjuZM1ASsuIeDpmmIZPeLPl',root_url='https://pitt.co1.qualtrics.com')
+# allsurveys<-getSurveys() 
+# allsurveys[ head(rev(order(allsurveys$lastModified) ),n=2),c('name','id') ]
+
+# 'PET/fMRI Screening: Adults'
+# adultScreen <- getSurvey('SV_1KPNjDDpbyRN7et', root_url='https://pitt.co1.qualtrics.com')
+
+# SV_7Qf9MM8lTG7fB0p # Adult Male Battery
+# SV_cBjZLr21bjgtt0V # Adult Female Battery
+adult_M <- getSurvey('SV_7Qf9MM8lTG7fB0p', root_url='https://pitt.co1.qualtrics.com')
+adult_F <- getSurvey('SV_cBjZLr21bjgtt0V', root_url='https://pitt.co1.qualtrics.com')
+
+
+
