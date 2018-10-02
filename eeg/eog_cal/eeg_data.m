bd4d4ba886aebc2e766369a5bf06a023dc7dc1e8
WillForan <willforan@gmail.com>
2018-10-02 12:46:44 -0400

    add subject selector, take multiple tasknames (as cell)

--- eeg/eog_cal/eeg_data.m
+++ eeg/eog_cal/eeg_data.m
@@ -1,18 +1,43 @@
 function [subj_struct] = eeg_data(taskname, channels, varargin)
-%EEG_DATA get raw eeg into matlab datas tructure
-%  USAGE: d = eeg_data('ANTI', {'Status','horz_eye'})
-%         cal = eeg_data('eyecal',{'Status','horz_eye'})
-%   provide a task, a lit of channels, and optionaly a processing level
-%  outputs a struct with field id, and d (data matrix)
+%EEG_DATA get raw eeg into matlab data structure
+%  eeg_data(taskname|fileglob|abspath|cell, channels[, 'subjs', {subj, subj}])
+%   provide 
+%     1) a way to find .bdf eeg files (taskname, abs path, glob, or cell of any of those)
+%         *  use cell for multiple tasks, see `cals=` example usage
+%     2) a cell of channels names
+%         * 'horz_eye' gives uniform name to left (EX3,EG3,FT7) and right eye
+%         * 'Status' is adjusted to get tiggers 0-255
+%     3) optionaly: 'subjs', subjlist cell
+%        eeg_data('MGS','subjs',{'11676','10195_20180201'})
+%   outputs a struct array with Fs, id, file, and channel data fields
+%
+%  USAGE:
+%   ch={'Status','horz_eye'};
+%   d = eeg_data('ANTI', ch,'subjs',{'11676','10195_20180201'});
+%   cal = eeg_data('eyecal',ch)
+%   cals = eeg_data({'eyecal','EOG'},ch)
+%   cal11451 = eeg_data('11451_2*_EOGCalib.bdf',ch)
+%    N.B. for one file, use  bdf_read_chnl(file, ch [,header])
+%     f='/Volumes/Hera/Raw/EEG/7TBrainMech/11676_20180821/11676_20180821_ANTI.bdf'
+%     bdf_read_chnl(f, ch)
+%    for all header information use fieldtrip:
+%     h=ft_read_header(f)
+%
+
+%% be helpful when given bad params
+if(nargin<1), help('eeg_data'); error("need task and channels"); end
+if(nargin==1)
+    warning(['no channels given! returning subject list instead of data; '...
+        'consider find_bdf(''' taskname ''')']);
+    subj_struct = find_bdf(taskname,varargin{:});
+    return
+end
 
 ft_test % check if we have fieldtrip
 
-root='/Volumes/Hera/Raw/EEG/7TBrainMech/';
-
-%% get all files like *Taskname*
-bdf_files = dir(fullfile(root,'1*_2*',['*' taskname '*.bdf'])); %TODO: preproc level
-bdf_files = arrayfun(@(x) fullfile(x.folder,x.name), bdf_files, 'UniformOutput',0);
-if length(bdf_files) < 1; error(['no files matching task' taskname]); end
+%% get all files
+bdf_files = find_bdf(taskname,varargin{:});
+if length(bdf_files) < 1; error(['no files matching task ' taskname]); end
 
 %% extract luna_date (5 digits _ 8 digits)
 subjids = cellfun(@(x) regexp(x,'\d{5}+_\d{8}+','once','match'),bdf_files,'UniformOutput',0);
---
+++ eeg/eog_cal/find_bdf.m
@@ -0,0 +1,54 @@
+function bdf_files = find_bdf(taskname,varargin)
+%FIND_BDF  find bdf files for 7TBrainMech task (synced from box)
+% USAGE: find_bdf('MGS','subjs',{'11676','10195_20180201'})
+
+%% cell of tasks => recursive calls to find_bdf
+% get each task individually, then combine
+if(isa(taskname,'cell'))
+    alltasks  = cellfun(@(x) find_bdf(x,varargin{:}), taskname, 'UniformOutput',0);
+    bdf_files = vertcat(alltasks{:});
+    return
+end
+
+%% figure out what to search for
+% taskname could be 
+%  1) full path to a single subject (or own glob)
+%  2) specific glob for all subjects
+%  3) a simple task pattern
+root='/Volumes/Hera/Raw/EEG/7TBrainMech/';
+if contains(taskname,'/')
+    if ~ contains(taskname,root)
+        warning(['you gave a path but it doesnt include the root dir (' ...
+            root ')']); 
+    end
+    searchpath=taskname;
+elseif contains(taskname,'*')
+    warning('using your glob instead of default');
+    searchpath=fullfile(root,'1*_2*',taskname);
+else
+    searchpath=fullfile(root,'1*_2*',['*' taskname ]);
+end
+% add extension if needed
+if isempty(regexp(taskname,'.bdf$','once'))
+ searchpath=[searchpath '*.bdf'];
+end
+    
+
+%% get all files
+bdf_files = dir(searchpath);
+bdf_files = arrayfun(@(x) fullfile(x.folder,x.name), bdf_files, 'UniformOutput',0);
+
+%% limit to given 'subjs'
+% get idex of 'subjects', look one past that
+% use contains against each list
+subjidx = find(cellfun( @(x) isa(x,'char') && strcmp(x,'subjs'), varargin));
+if ~isempty(subjidx)
+    subjlist=varargin{subjidx+1};
+    subj_in_list = cellfun(@(subj) contains(bdf_files,['/' subj]), subjlist, 'UniformOutput', 0);
+    list_idx = sum(cell2mat(subj_in_list),2) > 0;
+    bdf_files = bdf_files(list_idx);
+end
+
+
+end
+
