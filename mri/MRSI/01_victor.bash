01dc6c8c01b43f7af12d6fe337a1569c17d91a6b
WillForan <willforan@gmail.com>
2018-12-06 16:15:23 -0500

    luna-ize paths

--- mri/MRSI/01_victor.bash
+++ mri/MRSI/01_victor.bash
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 ################################## USER INPUT ##################################
-data_dir=/Users/victor/Desktop/Data/Patients/7TMB/20160115TBI  # directory where MPRAGE/SCOUT images in Nifty format and ‘Processed’ directory are located in
-filename_mprage=mprage.nii  # file name of MPRAGE
-filename_scout=scout.nii  # file name of SCOUT
-matlabcode_dir=/Users/victor/Desktop/Parcellation/Codes_yj  # directory where all the MATLAB codes for this script are located in
-spm_dir=/Users/victor/spm12
+# directory where all the MATLAB codes for this script are located in
+matlabcode_dir=$(cd $(dirname $0);pwd)/Codes_yj/
+spm_dir=/opt/ni_tools/matlab_toolboxes/spm12/
 
 flair_flag=0 # 1 if FLAIR exists, 0 if not
 #filename_flair=20141028_154029AXIALFLAIRs007a1001.nii # comment out this line if FLAIR doesn't exist
@@ -24,6 +22,27 @@ thresh_total=0.75  # GM + WM > thresh_total (should be between 0 and 1)
 thresh_maxtissue=0.5  # Fraction of Max Region > thresh_maxtissue (should be between 0 and 1)
 
 #filename_MRSI=spreadsheet  # MRSI excel file name after excluding '_SI*'
+
+################################################################################
+
+
+########################### script input #######################################
+[ $# -eq 0 ] && echo "USAGE: $0 subj_date scout_slice_num" && exit 1
+
+subj_date=$1
+subj=${s##_*}
+FSdir=/Volumes/Hera/Projects/7TBrainMech/FS/$subj/ 
+filename_scout="/Volumes/Hera/Projects/7TBrainMech/subjs/$subj_date/slice_PFC/slice_pfc.nii.gz"
+filename_mprage="/Volumes/Hera/Projects/7TBrainMech/pipelines/MHT1_2mm/$subj_date/mprage.nii.gz"
+#filename_mprage="/Volumes/Hera/Projects/7TBrainMech/subjs/10129_20180917/slice_PFC/ppt1/mprage.nii.gz"
+
+# TODO: do these have to be .nii instead of .nii.gz ?
+
+[ ! -r $FSdir ] && echo "$subj_date: no FS. run ../FS/002_fromPSC.bash" && exit 1
+[ ! -r $filename_scount ] && echo "$subj_date: no scount. run ../MRSI/01_get_slices.bash" && exit 1
+[ ! -r $filename_mprage ] && echo "$subj_date: no mprage. run ../010_preproc.bash" && exit 1
+
+data_dir="/Volumes/Hera/Projects/7TBrainMech/subjs/$subj_date/slice_PFC/MRSI"
 
 ################################################################################
 
@@ -57,15 +76,13 @@ echo "Deleting log file for linear regression"
 rm $log_file2
 fi
 
-### Freesurfer
-echo "Starting Freesurfer"
-recon-all -s FS -i $data_dir/$filename_mprage -all >$log_file1
 
 ### Divide ROI & Resize Scout to 1mm & Coregistration (MPRAGE to Scout) & linear regression
+[ ! -d $FSout_dir ] && mkdir -p $FSout_dir
 cd $FSout_dir
-mri_convert --in_type mgz --out_type nii --out_orientation RAS orig.mgz orig.nii
-mri_convert --in_type mgz --out_type nii --out_orientation RAS aparc+aseg.mgz aparc+aseg.nii
-mri_convert --in_type mgz --out_type nii --out_orientation RAS wmparc.mgz wmparc.nii
+mri_convert --in_type mgz --out_type nii --out_orientation RAS $FSdir/orig.mgz orig.nii
+mri_convert --in_type mgz --out_type nii --out_orientation RAS $FSdir/aparc+aseg.mgz aparc+aseg.nii
+mri_convert --in_type mgz --out_type nii --out_orientation RAS $FSdir/wmparc.mgz wmparc.nii
 
 if [ -d $ROI_dir ];then
     echo "Deleting parc_group directory"
@@ -80,5 +97,5 @@ fi
 3dcopy $FSout_dir/aparc+aseg.nii $ROI_dir/aparc+aseg.nii
 3dcopy $FSout_dir/wmparc.nii $ROI_dir/wmparc.nii
 
-/Applications/MATLAB_R2013b.app/bin/matlab -nodesktop -nosplash -r "nifti_dir=char(strcat('$nifti_dir')); addpath('$nifti_dir'); cd $matlabcode_dir; ROI_dir=char(strcat('$ROI_dir')); parc_grouping_ft_plusExtras('$(eval echo $ROI_dir)'); data_dir=char(strcat('$data_dir')); filename_scout=char(strcat('$filename_scout')); img_resize_ft('$(eval echo $data_dir)','$(eval echo $filename_scout)'); spm_dir=char(strcat('$spm_dir')); addpath('$spm_dir'); matlabcode_dir=char(strcat('$matlabcode_dir')); filename_flair=char(strcat('$filename_flair')); spm_registration_ROI_ft_plusExtras('$(eval echo $matlabcode_dir)','$(eval echo $ROI_dir)','$(eval echo $data_dir)','$(eval echo $filename_scout)',$flair_flag,'$(eval echo $filename_flair)'); parc_at_csi_multi_ft_plusExtras('$(eval echo $data_dir)',$flair_flag,'$(eval echo $filename_flair)',$scout_slice_num,$csi_size,$csi_FOV,$csi_thk,$scout_FOV,$scout_thk);" >$log_file2
+matlab -nodesktop -nosplash -r "nifti_dir=char(strcat('$nifti_dir')); addpath('$nifti_dir'); cd $matlabcode_dir; ROI_dir=char(strcat('$ROI_dir')); parc_grouping_ft_plusExtras('$(eval echo $ROI_dir)'); data_dir=char(strcat('$data_dir')); filename_scout=char(strcat('$filename_scout')); img_resize_ft('$(eval echo $data_dir)','$(eval echo $filename_scout)'); spm_dir=char(strcat('$spm_dir')); addpath('$spm_dir'); matlabcode_dir=char(strcat('$matlabcode_dir')); filename_flair=char(strcat('$filename_flair')); spm_registration_ROI_ft_plusExtras('$(eval echo $matlabcode_dir)','$(eval echo $ROI_dir)','$(eval echo $data_dir)','$(eval echo $filename_scout)',$flair_flag,'$(eval echo $filename_flair)'); parc_at_csi_multi_ft_plusExtras('$(eval echo $data_dir)',$flair_flag,'$(eval echo $filename_flair)',$scout_slice_num,$csi_size,$csi_FOV,$csi_thk,$scout_FOV,$scout_thk);" >$log_file2
 
