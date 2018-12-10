46e5f0df6bf579498cd82f755f02409d4aa7d58c
WillForan <willforan@gmail.com>
2018-12-10 10:00:12 -0500

    change CSI and scout FOVxThk

--- mri/MRSI/02_victor.bash
+++ mri/MRSI/02_victor.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -euo pipefail
+export AFNI_COMPRESSOR="" AFNI_NIFTI_TYPE_WARN=NO
 
 ################################## USER INPUT ##################################
 # directory where all the MATLAB codes for this script are located in
@@ -27,26 +28,30 @@ thresh_maxtissue=0.5  # Fraction of Max Region > thresh_maxtissue (should be bet
 
 
 ########################### script input #######################################
-[ $# -eq 0 ] && echo "USAGE: $0 subj_date scout_slice_num" && exit 1
+[ $# -eq 0 ] && echo "USAGE: $0 subj_date scout_slice_num # see /Volumes/Hera/Projects/7TBrainMech/pipelines/MHT1_2mm/ for subj list" && exit 1
 
 subj_date=$1
-subj=${s##_*}
+subj=${subj_date%%_*}
 FSdir=/Volumes/Hera/Projects/7TBrainMech/FS/$subj/ 
-filename_scout="/Volumes/Hera/Projects/7TBrainMech/subjs/$subj_date/slice_PFC/slice_pfc.nii.gz"
-filename_mprage="/Volumes/Hera/Projects/7TBrainMech/pipelines/MHT1_2mm/$subj_date/mprage.nii.gz"
+scout="/Volumes/Hera/Projects/7TBrainMech/subjs/$subj_date/slice_PFC/slice_pfc.nii.gz"
+mprage="/Volumes/Hera/Projects/7TBrainMech/pipelines/MHT1_2mm/$subj_date/mprage.nii.gz"
 #filename_mprage="/Volumes/Hera/Projects/7TBrainMech/subjs/10129_20180917/slice_PFC/ppt1/mprage.nii.gz"
 
-# TODO: do these have to be .nii instead of .nii.gz ?
-
-[ ! -r $FSdir ] && echo "$subj_date: no FS. run ../FS/002_fromPSC.bash" && exit 1
-[ ! -r $filename_scount ] && echo "$subj_date: no scount. run ../MRSI/01_get_slices.bash" && exit 1
-[ ! -r $filename_mprage ] && echo "$subj_date: no mprage. run ../010_preproc.bash" && exit 1
+[ ! -r $mprage ] && echo "$subj_date: no mprage ($mprage). run ../010_preproc.bash" && exit 1
+[ ! -r $scout ] && echo "$subj_date: no scout ($scout). run ../MRSI/01_get_slices.bash" && exit 1
+[ ! -r $FSdir ] && echo "$subj_date: no FS $FSdir. run ../FS/002_fromPSC.bash" && exit 1
 
 data_dir="/Volumes/Hera/Projects/7TBrainMech/subjs/$subj_date/slice_PFC/MRSI"
-
+[ ! -d $data_dir ] && mkdir $data_dir
 ################################################################################
 
 cd $data_dir
+# put files here like victor had them
+filename_scout=scout.nii
+filename_mprage=mprage.nii
+[ ! -r $filename_scout ] && 3dcopy $scout $filename_scout
+[ ! -r $filename_mprage ] && 3dcopy $mprage $filename_mprage
+
 log_file1=$data_dir/Log_Freesurfer.out
 log_file2=$data_dir/Log_Regression.out
 SUBJECTS_DIR=$data_dir
@@ -60,42 +65,50 @@ if [ "$flair_flag" -eq "0" ];then
     filename_flair=0
 fi
 
-### Delete existing files
-#if [ -e $log_file1 ];then
-#    echo "Deleting log file for Freesurfer"
-#    rm $log_file1
-#fi
-
-#if [ -d $SUBJECTS_DIR/FS ];then
-#echo "Deleting freesurfer directory"
-#rm -r $SUBJECTS_DIR/FS
-#fi
-
 if [ -e $log_file2 ];then
-echo "Deleting log file for linear regression"
-rm $log_file2
+   echo "Deleting log file for linear regression"
+   rm $log_file2
 fi
 
 
 ### Divide ROI & Resize Scout to 1mm & Coregistration (MPRAGE to Scout) & linear regression
 [ ! -d $FSout_dir ] && mkdir -p $FSout_dir
 cd $FSout_dir
-mri_convert --in_type mgz --out_type nii --out_orientation RAS $FSdir/orig.mgz orig.nii
-mri_convert --in_type mgz --out_type nii --out_orientation RAS $FSdir/aparc+aseg.mgz aparc+aseg.nii
-mri_convert --in_type mgz --out_type nii --out_orientation RAS $FSdir/wmparc.mgz wmparc.nii
+mri_convert --in_type mgz --out_type nii --out_orientation RAS $FSdir/mri/orig.mgz orig.nii
+mri_convert --in_type mgz --out_type nii --out_orientation RAS $FSdir/mri/aparc+aseg.mgz aparc+aseg.nii
+mri_convert --in_type mgz --out_type nii --out_orientation RAS $FSdir/mri/wmparc.mgz wmparc.nii
 
 if [ -d $ROI_dir ];then
     echo "Deleting parc_group directory"
     rm -r $ROI_dir
 fi
 
-# mkdir $ROI_dir
-if [ ! -d $ROI_dir ];then
-    mkdir -p $ROI_dir
-fi
+# mkdir $ROI_dir and copy thigs there
+[ ! -d $ROI_dir ] && mkdir -p $ROI_dir
+# AFNI converts NIFTI_datatype=8 (INT32) to FLOAT32
 3dcopy $FSout_dir/orig.nii $ROI_dir/orig.nii
 3dcopy $FSout_dir/aparc+aseg.nii $ROI_dir/aparc+aseg.nii
 3dcopy $FSout_dir/wmparc.nii $ROI_dir/wmparc.nii
 
-matlab -nodesktop -nosplash -r "nifti_dir=char(strcat('$nifti_dir')); addpath('$nifti_dir'); cd $matlabcode_dir; ROI_dir=char(strcat('$ROI_dir')); parc_grouping_ft_plusExtras('$(eval echo $ROI_dir)'); data_dir=char(strcat('$data_dir')); filename_scout=char(strcat('$filename_scout')); img_resize_ft('$(eval echo $data_dir)','$(eval echo $filename_scout)'); spm_dir=char(strcat('$spm_dir')); addpath('$spm_dir'); matlabcode_dir=char(strcat('$matlabcode_dir')); filename_flair=char(strcat('$filename_flair')); spm_registration_ROI_ft_plusExtras('$(eval echo $matlabcode_dir)','$(eval echo $ROI_dir)','$(eval echo $data_dir)','$(eval echo $filename_scout)',$flair_flag,'$(eval echo $filename_flair)'); parc_at_csi_multi_ft_plusExtras('$(eval echo $data_dir)',$flair_flag,'$(eval echo $filename_flair)',$scout_slice_num,$csi_size,$csi_FOV,$csi_thk,$scout_FOV,$scout_thk);" >$log_file2
+cat > fs_csi.m  <<EOF
+addpath('$nifti_dir');
+addpath('$spm_dir');
 
+matlabcode_dir='$matlabcode_dir';
+ROI_dir='$ROI_dir';
+data_dir='$data_dir';
+filename_scout='$filename_scout';
+
+cd(matlabcode_dir);
+parc_grouping_ft_plusExtras(ROI_dir);
+img_resize_ft('$data_dir','$filename_scout');
+spm_registration_ROI_ft_plusExtras(matlabcode_dir,ROI_dir,data_dir,filename_scout,$flair_flag,'$filename_flair');
+parc_at_csi_multi_ft_plusExtras('$data_dir',... datadir
+   $flair_flag,'$filename_flair', ... flair
+   $scout_slice_num,  ... slice number
+   $csi_size,  ... csi size
+   $csi_FOV,$csi_thk,    ... csi fov, thk
+   $scout_FOV,$scout_thk); ... scout FOV, thk
+EOF
+echo "# running $(pwd)/fs_csi.m"
+matlab -nodesktop -nosplash -r "try, fs_csi, end; quit" >$log_file2
