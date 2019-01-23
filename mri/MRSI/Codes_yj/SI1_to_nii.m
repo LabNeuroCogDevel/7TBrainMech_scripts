1f392dd62bf330447df788c1713f140fc7996a2c
WillForan <willforan@gmail.com>
2019-01-23 10:23:56 -0500

    ST1_to_nii can also just return matrix

--- mri/MRSI/Codes_yj/SI1_to_nii.m
+++ mri/MRSI/Codes_yj/SI1_to_nii.m
@@ -1,20 +1,26 @@
 function measures  = SI1_to_nii(csi_csv,tmpl_nii,outdir)
 % SI1_TO_NII Create nii images from spreadsheet
+%   if tmpl_nii and outdir are null, only return values read from sheet
 %% inputs
-
-
-% template nifti file to dump data into
-tmpl = load_untouch_nii(tmpl_nii);
-n = size(tmpl.img);
 
 % read in spreadsheet -- first 2 columns assumed to be 'Row' and 'Col'
 csivals = readtable(csi_csv);
+
+
+% template nifti file to dump data into
+if ~isempty(tmpl_nii)
+    tmpl = load_untouch_nii(tmpl_nii);
+    n = size(tmpl.img);
+else
+    n= [24, 24];
+end
+
 % convert row, column to single index
 i = sub2ind(n, csivals.Row, csivals.Col);
 
 %% for each measure in the csv file
 % extract matrix, add to nifti, save
-if ~exist(outdir,'dir'), mkdir(outdir), end
+if ~isempty(outdir) && ~exist(outdir,'dir'), mkdir(outdir), end
 measures = struct();
 for measure = csivals.Properties.VariableNames(3:end)
   name = measure{1};
@@ -31,7 +37,9 @@ for measure = csivals.Properties.VariableNames(3:end)
       name = [ name '_inv' ];
   end
   
-  csi2d_to_nii(m, fullfile(outdir, [name '.nii']), tmpl);
+  if ~isempty(outdir)
+     csi2d_to_nii(m, fullfile(outdir, [name '.nii']), tmpl);
+  end
   
   % keep around to check col row
   measures.(name) = m;
