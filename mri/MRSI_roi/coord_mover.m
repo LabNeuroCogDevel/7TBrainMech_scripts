ec5ed6fe5db8ef1d8fdeb42c21f3fe73cea2d8ee
WillForan <willforan@gmail.com>
2019-04-17 15:31:39 -0400

    gui to pick roi coordinates, file to set gui options of ifft

---
+++ mri/MRSI_roi/coord_mover.m
@@ -0,0 +1,312 @@
+function [f, coords] = coord_mover(ld8)
+%COORD_MOVER Summary of this function goes here
+%   Detailed explanation goes here
+
+  % example subj:
+  % ld8 = '11323_20180316';
+  n_rois = 12;
+  
+  %% find raw dir
+  % raw dir collects everything we need. depends on ./000_setupdirs.bash
+  rdir = sprintf('/Volumes/Hera/Projects/7TBrainMech/subjs/%s/slice_PFC/MRSI_roi/raw/', ld8);
+  if ~exist(rdir,'dir')
+     error('cannot read subject raw dir "%s"; run: ./000_setupdirs.bash %s', rdir, ld8)
+  end
+  
+  %% read mprage
+  mprage_file = [rdir '/rorig.nii'];
+  if ~exist(mprage_file,'file')
+     error('cannot read subject mprage (FS) in slice space "%s"; run: ./000_setupdirs.bash %s', mprage_file, ld8)
+  end
+  addpath('/Volumes/Hera/Projects/7TBrainMech/scripts/mri/MRSI/Codes_yj/NIfTI');
+  nii = load_untouch_nii(mprage_file);
+
+  %% read in coordinates
+  % like /Volumes/Hera/Projects/7TBrainMech/subjs/11323_20180316/slice_PFC/MRSI_roi/raw/slice_roi_CM_11323_20180316_16.txt
+  %  0	63  63
+  %  1	68  92
+  %  2	60  90
+  coords_file=sprintf('%s/slice_roi_CM_%s_16.txt',rdir,ld8);
+  % TODO: find slice number might not be 16
+  if ~exist(coords_file, 'file')
+     error('cannot read coord file "%s"; run: ./000_setupdirs.bash %s', coords_file, ld8)
+  end
+  coords = load(coords_file);
+  coords = coords(coords(:,1)~=0,:); % remove roi 0
+  if length(coords) ~= n_rois
+     warning('expected %d rois, have %d in %s', n_rois, length(coords), coords_file)
+     % replace missing rois with 0s
+     coords_new = zeros(n_rois,3); 
+     coords_new(:,1) = 1:n_rois;
+     coords_new(coords(:,1),:) = coords;
+     coords = coords_new;
+  end
+  
+  %% keep track of old coords
+  data.orig_coords = coords;
+  
+    %% set coords
+  % read in left and right
+  fid=fopen('./mni_coords.txt','r'); 
+  roi_mnicoord = strsplit(fread(fid,'*char')','\n');
+  fclose(fid);
+  roi_label = cellfun(@(x) regexprep(x,':.*',''), roi_mnicoord,'Un',0);
+  roi_label = roi_label(~cellfun(@isempty, roi_label));
+  if length(roi_label) ~= length(coords)
+      error('wrong number of rois in %s vs mni_coords.txt', coords_file)
+  end
+  
+  %% roi choices
+  
+  % color rois
+  % help from  https://www.mathworks.com/matlabcentral/answers/153064-change-color-of-each-individual-string-in-a-listbox
+  data.roi_colors = jet(n_rois);
+  rgb2hex = @(rgb) sprintf('%s', dec2hex(rgb.*255, 2 )');
+  html='<html><font id="%d" color="#%s">%s</font></html>';
+  roibox_str = arrayfun(@(i) ...
+                 sprintf(html, i, rgb2hex(data.roi_colors(i,:)), roi_label{i}), ...
+                 1:n_rois, 'Un',0);
+  
+  %% GUI figure
+
+  
+  
+  a_w = 216*2; a_h= 216*2;
+  % where should we show slice
+  z_mid = ceil(size(nii.img,3)/2);
+  z_above = z_mid + 5;
+  z_below = z_mid - 5;
+  % how to update axial images
+  funcs.mk_ax = @(x,y) axes('Units','Pixels', ...
+                       'Position', [x, y, a_w, a_h]);
+  funcs.mk_brain = @(ax, c) imagesc(ax, flipud(rot90(nii.img(:,:,c))));
+
+
+  % pos: dist from left, dist from bottom, width, hieght
+  f = figure('Visible','off','Position',[360 500 3*a_w, 2*a_h+40]);
+  
+  % put coords into gui data
+  data.coords = coords;
+  data.funcs = funcs;
+  data.z_mid = z_mid;
+  data.nii = nii;
+  guidata(f,data);
+  
+  % draw axial images
+  colormap bone
+  axial_above = make_axl_axis(10,       40, z_above, 'axabove', funcs);
+  axial_mid   = make_axl_axis(10+a_w,   40, z_mid,   'axmid',   funcs);
+  axial_below = make_axl_axis(10+a_w*2, 40, z_below, 'axbelow', funcs);
+  
+  % sagital
+  sagax  = axes('Units','Pixels', ...
+              'Position', [a_w*2, a_h+40, a_w, a_h]);
+  set(sagax,'Tag', 'sag');
+  
+  %coronal
+  corax  = axes('Units','Pixels', ...
+              'Position', [a_w, a_h+40, a_w, a_h]);
+  set(corax,'Tag', 'cor');
+  
+  % draw all the rectangles
+  draw_rectangles(f); %,[axial_above, axial_mid, axial_below])
+  
+  % box to select rois
+  roibox = uicontrol('Position',[20, a_h+40, floor(a_w/2), a_h-80], ...
+                     'String',roibox_str,...
+                     'Style','listbox', ...
+                     'Tag', 'roibox');
+  roibox.Callback = @(s,r) draw_rectangles(gcf);
+
+
+  % Buttons
+  rbtn   = uicontrol('Position',[20, a_h+20, 100, 20], ...
+                     'String','Reset',...
+                     'Tag', 'reset_button', ...
+                     'Callback', @reset_coords ...
+                     );
+                   
+  sbtn   = uicontrol('Position',[120, a_h+20, 100, 20], ...
+                     'String','Save',...
+                     'Tag', 'save_button', ...
+                     'Callback', @(s,e) msgbox('not implemented') ...
+                     );
+  lbtn  = uicontrol('Position',[220, a_h+20, 100, 20], ...
+                     'String','Load',...
+                     'Tag', 'load_button', ...
+                     'Callback', @(s,e) msgbox('not implemented') ...
+                     );
+  rbtn1  = uicontrol('Position',[320, a_h+20, 100, 20], ...
+                     'String','Reset1',...
+                     'Tag', 'reset1_button', ...
+                     'Callback', @reset_coords1 ...
+                     );                 
+                 
+  % roi positions original and new
+  % TODO: combine into one, add html color
+  dspcrd = uicontrol('Position',[20+a_w/3, a_h+40, a_w/3, a_h-80], ...
+                     'String',sprintf('%d %d %d\n', coords'),...
+                     'Style','text', ...
+                     'Tag', 'disp_orig');
+  crntcrd= uicontrol('Position',[a_w*2/3, a_h+40, a_w/3, a_h-80], ...
+                     'String',sprintf('%d %d %d\n', coords'),...
+                     'Style','text', ...
+                     'Tag', 'disp_current');
+  % each click changes the selected roi
+  %set(axial_mid,'NextPlot','add')
+  axial_above.ButtonDownFcn = @(src, event) set_coord(src, roibox);
+  axial_mid.ButtonDownFcn = @(src, event) set_coord(src, roibox);
+  axial_below.ButtonDownFcn = @(src, event) set_coord(src, roibox);
+
+  
+  
+  f.Visible = 'on';
+end
+
+
+function set_coord(src, roibox)
+  data = guidata(src);
+  xyz = get(src, 'CurrentPoint');
+  cur_roi = get(roibox, 'Value');
+  data.coords(cur_roi,2:3) = round(xyz(1,1:2));
+  guidata(src, data)
+  
+  root=groot;
+  draw_rectangles(root.CurrentFigure)
+  fprintf('roi %d: %d %d\n',cur_roi, data.coords(cur_roi,2:3))
+end
+
+function reset_coords1(varargin)
+  f=gcf;
+  cur_roi = get(findobj(f,'Tag','roibox'), 'Value');
+  data = guidata(f);
+  data.coords(cur_roi,:) = data.orig_coords(cur_roi,:);
+  guidata(f,data);
+  draw_rectangles(f);
+end
+
+function reset_coords(varargin)
+  f=gcf;
+  data = guidata(f);
+  data.coords = data.orig_coords;
+  guidata(f,data);
+  draw_rectangles(f);
+end
+
+function draw_rectangles(f,all_ax)
+   % rectantles
+   data = guidata(f);
+      
+   if ~isfield(data,'rects'), data.rects={}; end
+   
+   % get current figure if not provided
+   if nargin < 1
+       f=gcf;
+   end
+   % axis if we didn't explicly give them
+   if nargin < 2
+       axes = arrayfun(@(x) strncmp(x.Tag,'ax',2), f.Children);
+       all_ax = f.Children(axes);
+   end
+   
+   % remove any previous rectangles
+   for rr=data.rects
+       for r=rr{1}
+           delete(r)
+       end
+   end
+   
+   
+   nroi = size(data.roi_colors,1);
+   % set colors. current ROI is white
+   colors = data.roi_colors;
+   cur_roi = get(findobj(f,'Tag','roibox'), 'Value');
+   if(~isempty(cur_roi)), colors(cur_roi,:) = 1; end
+   
+   % update all axial
+   for ax_i = 1:numel(all_ax)
+       set(f,'CurrentAxes',all_ax(ax_i));
+       data.rects{ax_i} = arrayfun(@(i) rectangle(...
+                  'Position', [data.coords(i,2)-4.5,...
+                               data.coords(i,3)-4.5,...
+                               9,...
+                               9], ...
+                  'EdgeColor', colors(i,:), ...
+                  'LineWidth', 1,...
+                   'HitTest', 'off'), ...
+                 1:nroi);
+   end
+   
+   % update sag
+   s = findobj(f,'Tag','sag');
+   if(~isempty(s) && ~isempty(cur_roi))
+       roi_x = data.coords(cur_roi,2);
+       % update sag view
+       sag_p =imagesc(s,fliplr(rot90(squeeze(data.nii.img(roi_x,:,:)),3)));
+       set(sag_p,'HitTest', 'off');
+       set(s,'YDir', 'normal');
+       set(s,'Tag', 'sag'); % this gets lost every imagesc
+       
+       % draw boxes for rois in range
+       show_rois = find( abs(data.coords(:,2) - roi_x) < 10 );
+       set(f,'CurrentAxes',s);
+       data.rects{numel(all_ax)+1} = ...
+           arrayfun(@(i) rectangle(...
+                  'Position', [data.coords(i,3)-5, data.z_mid - 5,...
+                               9,                  10], ...
+                  'EdgeColor', colors(i,:), ...
+                  'LineWidth', 1,...
+                   'HitTest', 'off'), ...
+                 show_rois);
+   end
+   
+   % corronal
+   c = findobj(f,'Tag','cor');
+   if(~isempty(c) && ~isempty(cur_roi))
+       roi_y = data.coords(cur_roi,3);
+       % update sag view
+       sag_p =imagesc(c,fliplr(rot90(squeeze(data.nii.img(:,roi_y,:)),3)));
+       set(sag_p,'HitTest', 'off');
+       set(c,'YDir', 'normal');
+       set(c,'Tag', 'cor'); % this gets lost every imagesc
+       
+       % draw boxes for rois in range
+       show_rois = find( abs(data.coords(:,3) - roi_y) < 10 );
+       set(f,'CurrentAxes',c);
+       data.rects{numel(all_ax)+1} = ...
+           arrayfun(@(i) rectangle(...
+                  'Position', [data.coords(i,2)-5, data.z_mid - 5,...
+                               9,                  10], ...
+                  'EdgeColor', colors(i,:), ...
+                  'LineWidth', 1,...
+                   'HitTest', 'off'), ...
+                 show_rois);
+   end
+   
+   % update text
+   t = findobj(f,'Tag','disp_current');
+   if(~isempty(t))
+     t.String = sprintf('%d %d %d\n', data.coords');
+   end
+   
+   % put rects backinto gui data
+   guidata(f,data)
+end
+
+% things to do when reploting an axis
+function ax = make_axl_axis(x, y, z, tag, funcs)
+  ax   = funcs.mk_ax(x, y);
+  p = funcs.mk_brain(ax, z);
+  set(p,'HitTest', 'off');
+  set(ax,'YDir','normal');
+  set(ax,'Tag',tag);
+  text(ax,10,10,'Right','Color','White')
+
+  %set(ax,'HitTest','off')
+end
+
+function n=num_from_html(s)
+ id_n=regexp(s,'id="(\d+)"','tokens','once');
+ n = str2double(id_n{1});
+end
+
---
+++ mri/MRSI_roi/siarray_ifft_gui.m
@@ -0,0 +1,139 @@
+%
+% launch SVR1HFinal gui
+%
+% 20190412 - init
+function [f, coords] = siarray_ifft_gui(ld8)
+  % example subj:
+  % ld8 = '11323_20180316';
+  
+  n_rois = 12;
+  
+  %% find raw dir
+  % raw dir collects everything we need. depends on ./000_setupdirs.bash
+  rdir = sprintf('/Volumes/Hera/Projects/7TBrainMech/subjs/%s/slice_PFC/MRSI_roi/raw/', ld8);
+  if ~exist(rdir,'dir')
+     error('cannot read subject raw dir "%s"; run: ./000_setupdirs.bash %s', rdir, ld8)
+  end
+
+  %% read in coordinates
+  % like /Volumes/Hera/Projects/7TBrainMech/subjs/11323_20180316/slice_PFC/MRSI_roi/raw/slice_roi_CM_11323_20180316_16.txt
+  %  0	63  63
+  %  1	68  92
+  %  2	60  90
+  coords_file=sprintf('%s/slice_roi_CM_%s_16.txt',rdir,ld8);
+  % TODO: find slice number might not be 16
+  if ~exist(coords_file, 'file')
+     error('cannot read coord file "%s"; run: ./000_setupdirs.bash %s', coords_file, ld8)
+  end
+  coords = load(coords_file);
+  coords = coords(coords(:,1)~=0,:); % remove roi 0
+  if length(coords) ~= n_rois
+     warning('expected %d rois, have %d in %s', n_rois, length(coords), coords_file)
+     % replace missing rois with 0s
+     coords_new = zeros(n_rois,3); 
+     coords_new(:,1) = 1:n_rois;
+     coords_new(coords(:,1),:) = coords;
+     coords = coords_new;
+  end
+
+
+
+
+  %% launch figure
+  addpath('SVR1HFinal');
+  f=hgload('SVR1HFinal/SVR1H2015.fig');
+
+  %% set all values
+  % registration.out 7th mp2rage middle slice
+  % fake segementation by picking phony `seg.7` (copy of from mprage slice 7)
+  % reset angles
+
+  % function to set string of tag
+  setobj = @(x,y) set(findobj(f,'Tag',x),'String',y);
+
+  % set scout
+  setobj('DIRscout',rdir);
+  setobj('FOVscout','216');
+  setobj('RESscout','216');
+  % load, select mprage_middle.mat
+
+  % set seg, not used by probaly has to exist
+  setobj('DIRsegment',rdir);
+  setobj('FOVsegment','216');
+  setobj('RESsegment','216');
+  % load, select seg.7
+
+  % si
+  setobj('SIFileName',rdir)
+
+  % reset angles to empty
+  setobj('VOffset',0);
+  setobj('HOffset',0);
+  setobj('Angle',0);
+
+  
+  %% set coords
+  % read in left and right
+  fid=fopen('./mni_coords.txt','r'); 
+  side = textscan(fid, '%s %*[^\n]'); 
+  fclose(fid);
+  side=side{1}; % want first (and only) column
+  if length(side) ~= length(coords)
+      error('wrong number of rois in %s vs mni_coords.txt', coords_file)
+  end
+  % Left ACC: -8, 32, 23                           0 63 63                 
+  % Right ACC: 8, 32, 23                           1 68 92 
+  % ...    
+  % Right Middle Occipital Gyrus: 38, -71, -13    11 42 33
+  % Left Middle Occipital Gyrus: -42, -66, -10    12 86 26
+  d.L = 1;  d.R = 1; % intialize left and right count
+  for i = 1:length(coords)
+      lr=side{i}(1); % L or R
+      % TODO: confirm value is 'L' or 'R'
+      
+      % TODO: confirm row and col
+      % TODO: confirm orientation RAI vs LPI?
+      % set row
+      lbl = sprintf('%s%d%s', lr, d.(lr),'Row');
+      setobj(lbl, coords(i,2));
+      % and col      
+      lbl = sprintf('%s%d%s', lr, d.(lr),'Col');
+      setobj(lbl, coords(i,3));
+      
+      % enable radio
+      rdo = sprintf('%s%dOn', lr, d.(lr));
+      if coords(i,2) == 0 && coords(i,3) == 0
+          radval=0;
+      else
+          radval=1;
+      end
+      set(findobj(f,'Tag',rdo), 'Value',radval)
+      
+      d.(lr) = d.(lr) +1;
+  end
+  
+  if d.L ~= d.R
+      error('uneven number of left (%d) and right (%d) rois', d.L, d.R)
+  end
+  
+  %%
+  % 1. selection: write (puts .loc in siarray folder)
+  % 2. ifft (kspace transfrom)
+  % 3. recon coord
+
+  % WritePositions
+  % Reorient slice
+  % IFFT
+  % Recon Coords
+  % [enter] [enter]gg
+
+end
+  % see:  arrayfun(@(x) x.Tag,f.Children, 'Un', 0)
+  %       findobj(f,'-regexp', 'Tag', 'scout.*')
+  %       for c=f.Children', try, if strmatch('Reorient',c.String), c, end , end, end
+
+
+  %  h = guihandles(f);
+  % for hh=fieldnames(h)'; guidata(h.(hh{1})), end
+
+  % hgload ProcessVarian7TData1k/recent7TaSiemens.fig
