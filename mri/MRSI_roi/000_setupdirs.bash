856d11550d4b8ba6ffaa4b24a3e770913095a167
WillForan <willforan@gmail.com>
2019-04-30 15:26:07 -0400

    setupdirs looks in boxs PFC folder, want 20 not 21 strange slice numbers

--- mri/MRSI_roi/000_setupdirs.bash
+++ mri/MRSI_roi/000_setupdirs.bash
@@ -14,7 +14,7 @@ mni_atlas=$(pwd)/csi_rois_mni_MPRO_20190425.nii.gz
 
 if [ $# -le 0 ]; then
    cat <<-EOF
-   USAGE: 
+   USAGE:
     $0 lunaid_date   # specify single subject
     $0 all           # all subjects (print errors for missing)
     $0 have          # only what spreadsheet says we have
@@ -26,9 +26,10 @@ if [ $# -le 0 ]; then
 fi
 
 if [ $1 == "all" ]; then
-   ls /Volumes/Hera/Projects/7TBrainMech/subjs/*/slice_PFC/ | 
+   ls /Volumes/Hera/Projects/7TBrainMech/subjs/*/slice_PFC/ |
     perl -lne 'print $& if m/\d{5}_\d{8}/' |
-    xargs -n1 echo $0 
+    xargs -n1 $0
+    #xargs -n1 echo $0
    exit 0
 fi
 
@@ -38,8 +39,8 @@ if [ $1 == "have" ]; then
    [ ! -r "$statusfile" ] && echo "missing status files, run ../900_status.R" && exit 1
    Rio -e 'df$ld8[!is.na(df$csipfc_raw)&!is.na(df$ld8)]' < "$statusfile" |
     sed 's/\\n/\n/g' |
-    xargs -rn1 $0  
-    #xargs -rn1 echo $0 
+    xargs -rn1 $0
+    #xargs -rn1 echo $0
    exit 0
 fi
 
@@ -66,8 +67,8 @@ parc_res="/Volumes/Hera/Projects/7TBrainMech/subjs/$ld8/slice_PFC/MRSI/parc_grou
 slice_num=$(basename $MPRAGE | cut -f1 -d_)
 # afni needs zero based count
 slice_num_0=$((($slice_num - 1)))
-[ $slice_num_0 != 16 -a $slice_num_0 != 21 ] &&
-   echo "unexpected slice number zero-based slice index '$slice_num' (16 or 20), from $MPRAGE" && exit 1
+[ $slice_num_0 != 16 -a $slice_num_0 != 20 ] &&
+   echo "unexpected slice number zero-based slice index '$slice_num' ($slice_num_0 != 16 or 20), from $MPRAGE" && exit 1
 
 ## check final output -- no need to run if we already have it
 sdir=/Volumes/Hera/Projects/7TBrainMech/subjs/$ld8/slice_PFC/MRSI_roi/raw
@@ -85,13 +86,25 @@ done
 ## raw PFCCSI data
 # NB. when we get hipocampus, will cause issues
 rawdir="/Volumes/Hera/Raw/MRprojects/7TBrainMech/$MRID"
+boxsiarray="/Volumes/Hera/Raw/MRprojects/7TBrainMech/MRSI_BrainMechR01/PFC_siarray/$MRID"
 SIARRAY="$(
   find $rawdir -maxdepth 1 -type d,l -iname '*CSI*' -print0 |
    xargs -I{} -r0n1 find "{}" -maxdepth 1 -type f,l -iname 'siarray.*' -print -quit |
    sed 1q |
    xargs -r dirname
 )"
-[ -z "$SIARRAY" -o ! -d "$SIARRAY" ] && echo "cannot find si.array files in $rawdir; get data from Hoby!" && exit 1
+# nothing in raw data, try box
+[ -z "$SIARRAY" ] &&
+  SIARRAY="$(
+     find "$boxsiarray" -maxdepth 1 -type f,l -iname 'siarray.*' -print -quit |
+     sed 1q |
+     xargs -r dirname
+  )"
+
+if [ -z "$SIARRAY" -o ! -d "$SIARRAY" ]; then
+   echo "cannot find siarray files in $rawdir or $boxsiarray; get data from Hoby or Victor (../001_rsync_MRSI_from_box.bash)!"
+   exit 1
+fi
 
 # put links of files into a directory for easy viewing in matlab gui
 echo $ld8 $SIARRAY $MPRAGE
@@ -104,7 +117,7 @@ cd $sdir
 
 # warp our mni roi atlas to slice space (mni->t1->slice)
 outimg=csi_rois_slice_$ld8.nii.gz
-if [ ! -r $outimg ]; then 
+if [ ! -r $outimg ]; then
    cmd="applywarp -o $outimg -i $mni_atlas -r $pfc_ref -w $mni_to_t1 --postmat=$t1_to_pfc --interp=nn"
    eval "$cmd"
    3dNotes -h "$cmd # $0 $@" $outimg
@@ -115,7 +128,7 @@ fi
 # 3dcalc -a "$outimg" -expr "amongst(k,$slice_num_0+1,$slice_num_0, $slice_num_0-1)*a" -prefix csi_rois_slice_${ld8}_middle.nii.gz -overwrite
 3dcalc -a "$outimg" -expr "equals(k,$slice_num_0)*a" -prefix csi_rois_slice_${ld8}_middle.nii.gz -overwrite
 res_img=csi_rois_slice_${ld8}_middle_216x216.nii.gz
-[ ! -r $res_img ] && 
+[ ! -r $res_img ] &&
   3dresample -inset csi_rois_slice_${ld8}_middle.nii.gz \
      -master $parc_res \
      -prefix $res_img -rmode NN
@@ -126,7 +139,7 @@ AFNI_COMPRESSOR="" 3dMean -prefix gm_sum.nii -overwrite -sum $(dirname $parc_res
 
 
 # get the center of mass coordinates on the zero-indexed center slice (likely 16)
-3dCM -local_ijk -all_rois $res_img | 
+3dCM -local_ijk -all_rois $res_img |
   egrep '^[0-9]|#ROI'|
   paste - - |
   cut -f2-4 -d" " |
