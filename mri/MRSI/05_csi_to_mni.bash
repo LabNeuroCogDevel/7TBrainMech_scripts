2369e8823893a078643b9f2e517cc01513071b3f
WillForan <willforan@gmail.com>
2019-09-11 12:11:25 -0400

    add labels back to csi in mni

--- mri/MRSI/05_csi_to_mni.bash
+++ mri/MRSI/05_csi_to_mni.bash
@@ -14,12 +14,13 @@ for mdir in /Volumes/Hera/Projects/7TBrainMech/subjs/*/slice_PFC/; do
    [ ! -r $csi -o ! -r $pfc ] && echo "$mdir: missing slice_pfc or all_csi!" >&2 && continue
    [ ! -r ppt1/template_brain.nii -o ! -r ppt1/mprage_warpcoef.nii.gz ] && echo "$mdir/ppt1: missing mprage preprocessing" >&2 && continue
    [ ! -r $warpmat ] && echo "$mdir/$warpmat DNE. need to run MRSI_ROI scripts (../MRSI_roi/050_ROIs.bash)" >&2 && continue
+   labels="$(3dinfo -label $csi | sed 's/\[0\]//g;s/\.\?n\?i*|/ /g')"
    cmd="
       3dresample -inset $csi -master $pfc -prefix allcsi_tmp_large.nii.gz;
-      applywarp -i allcsi_tmp_large.nii.gz -o $final_csi_mni -r ppt1/template_brain.nii -w ppt1/mprage_warpcoef.nii.gz  --premat=$warpmat"
+      applywarp -i allcsi_tmp_large.nii.gz -o $final_csi_mni -r ppt1/template_brain.nii -w ppt1/mprage_warpcoef.nii.gz  --premat=$warpmat;
+      3drefit -relabel_all_str '$labels' $final_csi_mni;"
    echo -e "$(pwd)\n$cmd"
    eval $cmd
    [ -r allcsi_tmp_large.nii.gz ] && rm allcsi_tmp_large.nii.gz
-   [ -r $final_csi_mni ] && 3dNotes -h "$0 $cmd" $final_csi_mni
-
+   [ -r $final_csi_mni ] && 3dNotes -h "$0 $cmd" $final_csi_mni 
 done
