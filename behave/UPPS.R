271e4112612b3cba677f92accfac49124f415593
WillForan <willforan@gmail.com>
2020-02-24 13:04:23 -0500

    ✨ write out upps score for 7T qualtrics

--- behave/UPPS.R
+++ behave/UPPS.R
@@ -1,13 +1,15 @@
 library(dplyr)
 l <- Sys.glob('/Volumes/L/bea_res/Data/Temporary Raw Data/7T/*/*_selfreport.csv')
-extract_upps<-function(f){
+extract_upps<-function(f) {
    d <- read.csv(f)
    # find first question
    i <- grep(pattern="I have a reserved and cautious", t(d[1,]), ignore.case=T)[1]
    # have 59 questsions
-   rng <- i:(i+59)
+   rng <- i:(i+58)
    # extract data
+   # if there are multiple rows, the last is what we want (other is maybe question metadata)
    r <- d[nrow(d),rng]
+   
    # remove "INSTRUCTIONS.... - " from question names
    names(r) <- unname(gsub('.* - ','',t(d[1,rng]))) 
    return(r)
@@ -15,9 +17,12 @@ extract_upps<-function(f){
 upps <- lapply(l,extract_upps) %>% bind_rows()
 
 # extract luna_date
-upps$id <- sapply(l,stringr::str_extract,'\\d{5}_\\d{8}')
+newlevels <- c("Agree Strongly","Agree Some", "Disagree Some", "Disagree Strongly")
+uppsout <-
+    upps %>%
+    mutate_all(list(function(x) factor(x, levels=newlevels) %>% as.numeric)) %>%
+    mutate(id=sapply(l,stringr::str_extract,'\\d{5}_\\d{8}'))
 
-# TODO:
-# respones back to numbers
-# upps %>% mutate_all(as.factor( levels=("Agree Strongly","Agree Some", "Disagree Some", "Disagree Strongly"))
-# add subjectid back
+# uppsp_scoring expects colnames to be e.g. "1.I have a resev...." which is what we have!
+upps_scored <- LNCDR::uppsp_scoring(uppsout) %>% mutate(id=uppsout$id)
+write.csv(upps_scored, 'txt/upps_scored.csv',row.names=F)
