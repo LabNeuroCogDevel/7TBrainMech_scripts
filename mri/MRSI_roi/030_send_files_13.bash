b1f2d2e7dab3623c14fb9b9838c681ceaecb49ba
WillForan <willforan@gmail.com>
2020-03-12 16:47:50 -0400

    ✨ MRSI send files: can read subjlist from file

--- mri/MRSI_roi/030_send_files_13.bash
+++ mri/MRSI_roi/030_send_files_13.bash
@@ -13,20 +13,42 @@ env |grep -q "^DRYRUN=" && DRYRUN="echo" || DRYRUN=""
 # 20200225 - copy from 030_send_files_24.bash 
 
 nspecfiles=13  # number of coords
-newer=2020-02-24 # didn't start creating 13 ROI spectrum files until after feb 24
-                 # will also check already recieved files
-
-# change me
-# version=20200224 # outputfoldername version
-version=20200311 # outputfoldername version
-
-#[[ $# -eq 0 || "$1" =~ help|-h ]] && echo "change hardcoded date in $0; change output name maybe" && exit 1
-
-root_out=spectrum_out/${version}_${nspecfiles}specs
-[ ! -d $root_out ] && mkdir -p $root_out
+atlas=13MP20200207
+INPICKONLY=1   # only copy if in pick_coords.txt?, ="" to disable
 
 subj_root=$(cd $(pwd)/../../../subjs/; pwd)
-find $subj_root/*/slice_PFC/MRSI_roi/raw/ -iname '*spectrum.[0-9]*' -newermt $newer -exec stat -c "%y %n" {} \+ |
+# change me - outputfoldername version
+# version=20200224      # init
+# version=20200311      # second pass
+version=$(date +%Y%m%d) 
+
+_findspecs(){
+   # default to "newer" find
+   local newer="-newermt $2"
+   local id="*"
+   [ "$1" == "id" ] && id="$2" && newer=""
+   find $subj_root/$id/slice_PFC/MRSI_roi/raw/ -iname '*spectrum.[0-9]*' $newer -exec stat -c "%y %n" {} \+ 
+}
+usage(){ echo "USAGE: $0 new OR $0 20yy-mm-dd OR $0 ld8list.txt" && exit 1; }
+
+[[ $# -eq 0 || "$1" =~ help|-h ]] && usage
+if [[ "$1" == "new" ]]; then
+   # didn't start creating 13 ROI spectrum files until after feb 24
+   # will also check already recieved files
+   findspecs(){ _findspecs newer 2020-02-24;}
+elif [[ "$1" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
+   findspecs(){ _findspecs newer $1;}
+elif [ -r "$1" ]; then
+   subjlist="$1"
+   findspecs(){ for ld8 in $(cat $subjlist|xargs ld8); do _findspecs id $ld8; done; }
+else
+   usage
+fi
+
+root_out=spectrum_out/${version}_${atlas}specs
+[ ! -d $root_out ] && mkdir -p $root_out
+
+findspecs |
    grep -v 'spectrum.0.0' |
    # eg 2019-11-26 17:03:31.155522625 -0500 /Volumes/Hera/Projects/7TBrainMech/subjs/11752_20190315/slice_PFC/MRSI_roi/raw/spectrum.100.121
    sed 's/-//;s/-//' | # 2019-11-21 -> 20191121
@@ -34,15 +56,23 @@ find $subj_root/*/slice_PFC/MRSI_roi/raw/ -iname '*spectrum.[0-9]*' -newermt $ne
    perl -slane 'next unless m:(/.*(\d{5}_\d{8}).*):; print "$2 $1 $F[0]" '|
 while read ld8 f d; do
    ld8mr=$(grep $ld8 ../MRSI/txt/ids.txt |sed 's/ /-/;1q') # could maybe have done this with 'join'
-   [ -z "$ld8mr" ] && echo "unknown MRID for $ld8 " && continue
+   [ -z "$ld8mr" ] && echo "# unknown MRID for $ld8" >&2 && continue
+   specyx=$(basename $f)
 
    # test where we might have downloaded
-   prevlcm="$( (ls /Volumes/Hera/Raw/MRprojects/7TBrainMech/MRSI_BrainMechR01/*/$ld8mr/$(basename $f).dir 2>/dev/null || echo -n) |sed 1q)"
-   test -n "$prevlcm" -a -r "$prevlcm" && echo "have $_" && continue
+   prevlcm="$( (ls /Volumes/Hera/Raw/MRprojects/7TBrainMech/MRSI_BrainMechR01/*/$ld8mr/$specyx.dir 2>/dev/null || echo -n) |sed 1q)"
+   test -n "$prevlcm" -a -r "$prevlcm" && echo "# SKIP: have $_" >&2 && continue
 
    # test for were we want it
-   prevlcm=/Volumes/Hera/Projects/7TBrainMech/subjs/$ld8/slice_PFC/MRSI_roi/LCModel/v1/$(basename $f).dir 
-   test -r $prevlcm && echo "have $_" && continue
+   prevlcm=/Volumes/Hera/Projects/7TBrainMech/subjs/$ld8/slice_PFC/MRSI_roi/LCModel/v1/$specyx.dir 
+   test -r $prevlcm && echo "# SKIP: have $_" >&2 && continue
+
+   if [ -n "$INPICKONLY" ]; then
+      xy=$(echo $specyx| awk -F\. '{print $3"\t"$2}')
+      ! grep -q "$xy" /Volumes/Hera/Projects/7TBrainMech/subjs/$ld8/slice_PFC/MRSI_roi/$atlas/*/picked_coords.txt &&
+         echo "# SKIP: $ld8 $(basename $f) '$xy' not in $_ (INPICKONLY='' to ignore)" >&2 && 
+         continue
+   fi
 
    # link to directory
    outdir="$root_out/$ld8mr"
@@ -53,7 +83,8 @@ while read ld8 f d; do
    continue
 done
 
-[ -n $DRYRUN ] && zip -r $root_out.zip $root_out
+[ -n "$DRYRUN" ] && exit
+zip -r $root_out.zip $root_out
 
 #check
 for d in $root_out/1*/; do
