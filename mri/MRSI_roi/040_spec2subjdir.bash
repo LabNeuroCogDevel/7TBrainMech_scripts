ef98ad571c9441d1c51a654e9e491a2232ae0ec4
WillForan <willforan@gmail.com>
2020-03-19 10:57:24 -0400

    🐛 skip copy only if file exists, not just if dir exists

--- mri/MRSI_roi/040_spec2subjdir.bash
+++ mri/MRSI_roi/040_spec2subjdir.bash
@@ -4,9 +4,13 @@ trap 'e=$?; [ $e -ne 0 ] && echo "$0 exited in error"' EXIT
 
 # set verbose if not in environment
 env |grep -q '^VERBOSE=' || VERBOSE=""
+# ONLYID to tust just one luna_date
+env |grep -q '^ONLYID=' || ONLYID=""
 
 # CHANGE ME
-unzipfolder="/Volumes/Hera/Raw/MRprojects/7TBrainMech/MRSI_BrainMechR01/20200224_13specs_processed/"
+#unzipfolder="/Volumes/Hera/Raw/MRprojects/7TBrainMech/MRSI_BrainMechR01/20200224_13specs_processed/"
+#unzipfolder="/Volumes/Hera/Raw/MRprojects/7TBrainMech/MRSI_BrainMechR01/20200311_13specs_processed/"
+unzipfolder="/Volumes/Hera/Raw/MRprojects/7TBrainMech/MRSI_BrainMechR01/2020*processed/"
 atlas=13MP20200207
 #
 # copy LCmodel output to subject MRSI_roi directory (MRSI_roi/LCModel/v1)
@@ -23,6 +27,7 @@ S="/Volumes/Hera/Projects/7TBrainMech/subjs/"
 for d in $unzipfolder/*/; do
    ld8=$(ld8 "$d")
    [ -z "$ld8" ] && echo "ERROR: no lunadate in $d" && continue
+   [ -n "$ONLYID" -a "$ONLYID" != "$ld8" ] && continue
    o=$S/$ld8/slice_PFC/MRSI_roi/$atlas
    [ ! -d $o ] && echo "ERROR: no out dir $o for $d" && continue
    c=$(find $o/ \
@@ -59,9 +64,12 @@ for d in $unzipfolder/*/; do
    test ! -r $M && mkdir -p $_
    test ! -r $M/${atlas}_picked_coords.txt && ln -s $c $_
    for kd in ${keep[@]}; do
-      [ -d $M/$kd ] && continue
-      echo "copying $M/$kd"
-      cp -r $d/$kd $M/
+      [ -r $M/$kd/spreadsheet.csv ] && continue
+      echo "copying to $M/$kd"
+      rsync -ravhi --size-only $d/$kd $M/
    done
+
+   test ${#keep[@]} -ne $(find $M -iname 'spreadsheet.csv'|wc -l) && 
+      echo "ERROR: $ld8 has $_ instead of expected ${#keep[@]} in $M (see $d)" || :
   
 done
