e3dd8d2ecb805a32e1a4b20bcecf8d3521002cfe
WillForan <willforan@gmail.com>
2020-04-02 12:50:30 -0400

    🐛 MRSI/gen_pdf fix bookmark generator. add page number variable

--- mri/MRSI_roi/gen_pdf.bash
+++ mri/MRSI_roi/gen_pdf.bash
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
 trap 'e=$?; [ $e -ne 0 ] && echo "$0 exited in error"' EXIT
+cd $(dirname $0)
 
 #
 # 20200402WF - init - make pdfs of first page
 #
 
+pg=1
 
 test -d /tmp/pdfs || mkdir $_
 find /Volumes/Hera/Raw/MRprojects/7TBrainMech/MRSI_BrainMechR01/2020*/*_20*/spectrum*.dir -type f -name csi.ps | while read f; do
-  out=/tmp/pdfs/$(ld8 $f)-$(echo $f|perl -lne 'print $1 if /spectrum.(\d+.\d+).dir/').pdf
+  out=/tmp/pdfs/$(ld8 $f)-$(echo $f|perl -lne 'print $1 if /spectrum.(\d+.\d+).dir/')-pg$pg.pdf
+  [ -r "$out" ] && continue
   echo $out
-  gs -q -dSAFER -dBATCH -dFirstPage=1 -dLastPage=1 -sDEVICE=pdfwrite -o $out $f
+  gs -q -dSAFER -dBATCH -dFirstPage=$pg -dLastPage=$pg -sDEVICE=pdfwrite -o "$out" $f
 done
-ls /tmp/pdfs/*pdf |
- sed 's:.*/::;s:.pdf::' |
- perl -lne 'print "[/Page 1 /View [/XYZ null null null] /Title ($_) /OUT pdfmark"' > /tmp/pdfs/bookmarks.ps
-gs -q -dSAFER -dBATCH -sDEVICE=pdfwrite -o csi_all1.pdf /tmp/pdfs/bookmarks.ps -f /tmp/pdfs/*pdf
+
+# make bookmark file
+ls /tmp/pdfs/*-pg$pg.pdf |
+ sed "s:.*/::;s:-pg$pg.pdf$::" |
+ perl -lne 'print "[/Page ", ++$i, " /View [/XYZ null null null] /Title ($_) /OUT pdfmark"' > /tmp/pdfs/bookmarks-pg$pg.ps
+
+echo "combining"
+[ ! -d pdf ] && mkdir $_
+gs -q -dSAFER -dBATCH -sDEVICE=pdfwrite -o pdf/csi_all$pg.pdf /tmp/pdfs/bookmarks-pg$pg.ps -f /tmp/pdfs/*pg$pg.pdf
 
