ad11f61475ccc481e12631c0738277d08c437b91
WillForan <willforan@gmail.com>
2020-04-23 11:02:02 -0400

    ✨ mrsi/pdfs: limit to new spectrum

--- mri/MRSI_roi/gen_pdf.bash
+++ mri/MRSI_roi/gen_pdf.bash
@@ -8,21 +8,23 @@ cd $(dirname $0)
 #
 
 pg=1
-
-test -d /tmp/pdfs || mkdir $_
-find /Volumes/Hera/Raw/MRprojects/7TBrainMech/MRSI_BrainMechR01/2020*/*_20*/spectrum*.dir -type f -name csi.ps | while read f; do
-  out=/tmp/pdfs/$(ld8 $f)-$(echo $f|perl -lne 'print $1 if /spectrum.(\d+.\d+).dir/')-pg$pg.pdf
+newer=2020-04-19
+tmpdir=$(mktemp -d /tmp/mrsi-pdfs-XXXX)
+test -d $tmpdir || mkdir $_
+find /Volumes/Hera/Raw/MRprojects/7TBrainMech/MRSI_BrainMechR01/20*/*_20*/spectrum*.dir -type f -name csi.ps -newermt $newer | while read f; do
+  out=$tmpdir/$(ld8 $f)-$(echo $f|perl -lne 'print $1 if /spectrum.(\d+.\d+).dir/')-pg$pg.pdf
   [ -r "$out" ] && continue
   echo $out
   gs -q -dSAFER -dBATCH -dFirstPage=$pg -dLastPage=$pg -sDEVICE=pdfwrite -o "$out" $f
 done
 
 # make bookmark file
-ls /tmp/pdfs/*-pg$pg.pdf |
+ls $tmpdir/*-pg$pg.pdf |
  sed "s:.*/::;s:-pg$pg.pdf$::" |
- perl -lne 'print "[/Page ", ++$i, " /View [/XYZ null null null] /Title ($_) /OUT pdfmark"' > /tmp/pdfs/bookmarks-pg$pg.ps
+ perl -lne 'print "[/Page ", ++$i, " /View [/XYZ null null null] /Title ($_) /OUT pdfmark"' > $tmpdir/bookmarks-pg$pg.ps
 
 echo "combining"
 [ ! -d pdf ] && mkdir $_
-gs -q -dSAFER -dBATCH -sDEVICE=pdfwrite -o pdf/csi_all$pg.pdf /tmp/pdfs/bookmarks-pg$pg.ps -f /tmp/pdfs/*pg$pg.pdf
+gs -q -dSAFER -dBATCH -sDEVICE=pdfwrite -o pdf/csi_all-gt$newer-pg$pg.pdf $tmpdir/bookmarks-pg$pg.ps -f $tmpdir/*pg$pg.pdf
 
+rm -r "$tmpdir"
