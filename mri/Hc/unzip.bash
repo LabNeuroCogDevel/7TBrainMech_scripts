b54d22771a9f12353d68879fce15b222ef5b1645
WillForan <willforan@gmail.com>
2020-05-20 19:08:06 -0400

    ✨ Hc: add 2019 to txt, track unzip

---
+++ mri/Hc/unzip.bash
@@ -0,0 +1,31 @@
+echo "NOT RUNNING. Here for documentation only"
+exit 1
+# this is just here for documentation. should never need to be rerun
+
+cd raw_zipdir/ # /Volumes/Hera/Raw/MRprojects/7TBrainMech/MRSI_BrainMechR01
+
+# 20200519 - 
+[ ! -d HPC/ProcessedHc_20200520_2019-Mar2020 ] &&
+   echo unzip -d HPC/ ProcessedHc_20200520_2019-Mar2020.zip
+
+# 20200520 - fetch old data kindly re-upload to shimlinux by victor!
+[ ! -d HPC/Processed103019/ ] &&
+   echo unzip -d HPC/ ProcessedHc_20190722-20190913.zip
+
+outdir=$(pwd)/HPC/ProcessedHc103019_fixed_ln
+test ! -d $outdir && mkdir $_
+# standardize naming: HPC/Processed103019/ 
+for f in $(pwd)/HPC/Processed103019/*/locations.txt; do
+   ! [[ $f =~ Luna([0-9]{4})([0-9]) ]] && echo "bad name $f" && continue
+   newid=2019${BASH_REMATCH[1]}Luna${BASH_REMATCH[2]}
+   echo $newid
+   cat $f | while read side roi r c; do
+      d=$(dirname $f)/spectrum.$r.$c.dir
+      [ ! -d $d ] && echo "no $d!" && continue
+      # new dir like
+      # spectrum.71.111.R.2.dir
+      new_d=$outdir/$newid/spectrum.$r.$c.$side.$roi.dir
+      test -d $(dirname $new_d) || mkdir $_
+      [ ! -e $new_d ] && ln -s $d $new_d
+   done
+done
