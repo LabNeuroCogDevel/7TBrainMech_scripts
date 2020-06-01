222e708ad8661155aa60a2a1b3cf5229dfa71a20
WillForan <willforan@gmail.com>
2020-06-01 14:18:01 -0400

    🚧 puberty: write out file, but its only for females

--- behave/Puberty.R
+++ behave/Puberty.R
@@ -1,4 +1,6 @@
 #!/usr/bin/env Rscript
+# pull puberty data from selfreport.csv sheets gernerated by RAs from qualtrics
+# 20200601WF (OR) - init
 library(dplyr)
 
 
@@ -9,7 +11,7 @@ extract_pub<-function(f, qpatt, n_qs) {
    # find first question
    i <- grep(pattern=qpatt, t(d[1,]), ignore.case=T, perl=T)
    if(length(i) <= 0L) {
-      warning(sprintf("%d columns matched '%s' for '%s'",length(i),frst_qstr, f))
+      warning(sprintf("%d columns matched '%s' for '%s'",length(i),qpatt, f))
       return(NULL)
    }
    i<-i[1]
@@ -26,5 +28,12 @@ extract_pub<-function(f, qpatt, n_qs) {
 frst_qstr <- "How old were you when you started having periods?" 
 n_qs <- 5
 pubertyl <- lapply(l,extract_pub, qpatt=frst_qstr,n_qs=n_qs)
-puberty <-  pubertyl %>%  bind_rows()
+puberty_female <-  pubertyl %>%  bind_rows()
+write.csv(puberty_female,"txt/Puberty.csv")
+
+
+# TODO: males do not have data!?
+
+extract_pub(l[[3]], qpatt='your growth in height',n_qs=20)
+pubertyl_m <- lapply(l,extract_pub, qpatt='your growth in height',n_qs=n_qs)
 
