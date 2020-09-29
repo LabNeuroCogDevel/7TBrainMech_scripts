6889d6e4d8a09b82b1acc1c03f8f14b946d81f0e
WillForan <willforan@gmail.com>
2020-09-29 11:03:11 -0400

    🐛 hera: fix for loop shape; return found path

--- eeg/eog_cal/hera.m
+++ eeg/eog_cal/hera.m
@@ -1,24 +1,29 @@
 function savedir = hera(varargin)
+   % HERA - find data root direcotry (probably /Volumes/Hera)
+   % Hera on lab servers is in /Volumes (also see  'sshfs r:/Volumes /Volumes')
+   % but on macOS mounting all /Volumes isn't a good idea
+   % so will have variable Hera locations. enumerated in 'places'
+
    if ispc
        savedir = fullfile('H:/',varargin{:});
        return
    end
 
-   % Hera on rhea is in volumes (also see e.g. 'sshfs r:/Volumes /Volulmes')
-   % but on macOS mounting all /Volumes isn't possible
-   % so will have variable Hera locations. enumerated here
+   % EDIT ME: add paths (vertically!) as they are needed
+   % vertical for formating. but also see transpose in for loop def
    places = {
       '/Volumes/Hera'
       '/Users/fcalabro/mnt/rhea/Volumes/Hera'};
    rootdir = '';
-   for p=places
+   for p=places'
       if exist(p{1}, 'dir')
          rootdir=p{1};
          break
       end
    end
+
    if isempty(rootdir), error('cannot find hera route'), end
-   savedir = fullfile('/Volumes/Hera/',varargin{:});
+   savedir = fullfile(rootdir,varargin{:});
    
    % for host specfic could try
    % [err, host] = system('hostname');
