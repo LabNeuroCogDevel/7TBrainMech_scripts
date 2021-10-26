08a09c108e776423bc3e4a3b8aaf2903dac11928
WillForan <willforan@gmail.com>
2021-10-25 20:01:24 -0400

    🐛 FS: ignore bold.nii.gz; add DRYRUN, be more verbose

--- mri/FS/001_runlocal.bash
+++ mri/FS/001_runlocal.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+[ -v DRYRUN ] && DRYRUN=echo || DRYRUN=
 
 export SUBJECTS_DIR="/Volumes/Hera/preproc/7TBrainMech_rest/FS"
 bidsroot="/Volumes/Hera/Projects/7TBrainMech/BIDS/"
@@ -7,18 +8,22 @@ bidsroot="/Volumes/Hera/Projects/7TBrainMech/BIDS/"
 export MAXJOBS=15 WAITTIME=60 JOBCFGDIR="$(dirname $0)/.jobcfg"
 source /opt/ni_tools/lncdshell/utils/waitforjobs.sh # waituntildone waitforjobs
 
-find $bidsroot/sub* -iname '*T1*.nii.gz'| while read t1; do
+find $bidsroot/sub* -iname '*T1*.nii.gz' -not -iname '*lowres*' -not -iname '*bold.nii.gz' |
+   while read t1; do
    ld8=$(ld8 ${t1//\//_})
    [ -z "$ld8" ] && echo "ERROR: no id in $t1" && continue
+   [[ $ld8 == "11681_20181012" ]] && echo "# skipping $ld8, known bad!" && continue
    logfile=$SUBJECTS_DIR/$ld8/scripts/recon-all.log 
-   grep -q finished\ without $logfile 2>/dev/null && continue
+   grep -q finished\ without $logfile 2>/dev/null && echo "# complete $logfile" && continue
    pgrep -af "recon-all.*$ld8" && echo "$ld8: already running" && continue
    if [ ! -r $logfile ]; then
-      recon-all -subjid $ld8 -i $t1 -all &
+      $DRYRUN recon-all -subjid $ld8 -i $t1 -all &
    else
-      recon-all -subjid $ld8 -all -no-isrunning &
+      echo "# $t1"
+      $DRYRUN recon-all -subjid $ld8 -all -no-isrunning &
    fi
    waitforjobs
 done
 
 waituntildone
+wait
