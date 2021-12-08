0cd83f36ad858c525c5b4df0492b66383da547cb
WillForan <willforan@gmail.com>
2021-12-08 14:25:59 -0500

    ✨ track 013_tsnr.bash after looking at _ica also

---
+++ mri/013_tsnr.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+trap 'e=$?; [ $e -ne 0 ] && echo "$0 exited in error"' EXIT
+
+#
+# run native space tsnr on initial
+#  20200305WF  init
+#  20211208WF also run for MHRest_nost_ica (maybe primary rest preproc?)
+
+for f in /Volumes/Hera/preproc/7TBrainMech_{rest/MHRest_nost{,_ica}/1*_2*/,_mgsencmem/MHTask_nost/1*_2*/*}/wdkm_func.nii.gz; do
+   echo $f
+   cd $(dirname $f)
+   #ppf_tsnr -n -O _func.nii.gz -c || continue
+   ppf_tsnr -O wdkm_func.nii.gz || continue
+done
