1e8363ecd1a5ac23f9b791525f7b7f67893f7d6c
WillForan <willforan@gmail.com>
2022-01-19 09:32:29 -0500

    ✨ PFC autospec: show count of to-run (lines of ml script); capture missing NW

--- mri/MRSI_roi/021_mkspec_auto.sh
+++ mri/MRSI_roi/021_mkspec_auto.sh
@@ -8,6 +8,7 @@
 #    write to tempfile instead of launching a bunch of matlabs
 #    update to use lncd renamed matlab oneliner m->ml
 WHO="OR"; WHEN=2020-07-01 # 20200720
+WHO="NW"; WHEN=2020-10-01 # 20220118 # previously missing, back in time to catch
 WHO="AG"; WHEN=2021-10-01 # 20211103
 
 mkspecdir=$(cd $(dirname $0);pwd)
@@ -19,7 +20,7 @@ find /Volumes/Hera/Projects/7TBrainMech/subjs/*/slice_PFC/MRSI_roi/13MP20200207/
    -maxdepth 1 -newermt $WHEN -type d|
  while read d; do
   test 0 -ne $(ls $d/spectrum* 2>/dev/null| wc -l) &&
-     echo "#have $d $_" >&2 &&
+     echo "# have $d $_" >&2 &&
      continue
   test ! -r $d/sid3_picked_coords.txt  && echo "missing $_" >&2 && continue
   echo "cd('$mkspecdir'); mkspec_indir('$d');"
@@ -27,6 +28,7 @@ done  >> $tmp_ml
 
 if [ -n "$DRYRUN" ]; then
    echo "# $tmp_ml"
+   wc -l < $tmp_ml
    cat $tmp_ml
 else
    ml $tmp_ml
