8cd2d9596d94ce26753c68600b4c5decf5d28d14
WillForan <willforan@gmail.com>
2022-01-19 09:40:38 -0500

    💡 pfc needs_placment vs 001_inspect list

--- mri/MRSI_roi/needs_placement.bash
+++ mri/MRSI_roi/needs_placement.bash
@@ -5,6 +5,9 @@ trap 'e=$?; [ $e -ne 0 ] && echo "$0 exited in error"' EXIT
 #
 # quick list of sessions without mrsi placements
 #  20211025WF  init
+#  different from './001_inspect_setup.bash list'
+#     inspect_setup lists all 'raw' folders (exists)
+#     needs_placement shows where '13MP20200207' does not exist
 cd $(dirname $0)
 for d in ../../../subjs/1*_2*/slice_PFC/MRSI_roi/; do
    [ ! -r $d/13MP20200207 ] && ld8 $d || :;
