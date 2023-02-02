f9cfef369e59358c54bc1a454b58e4eb3b3014ac
WillForan <willforan@gmail.com>
2023-02-02 09:21:37 -0500

    fs_label_idv.R: refactor and add HBT

--- mri/Hc/fs_label_idv.R
+++ mri/Hc/fs_label_idv.R
@@ -12,35 +12,50 @@ file_unrotated_coord <- "hc_loc_unrotated.1d" # 12x5 coordinate unrotated from g
 
 # setwd("/Volumes/Hera/Projects/7TBrainMech/scripts/mri/Hc/spectrum/20220707Luna1")
 suppressPackageStartupMessages({library(dplyr);library(tidyr)});
-lut <- read.table("/opt/ni_tools/freesurfer/ASegStatsLUT.txt",
-                  col.names=c("FSroi","label", "r", "g", "b", "a")) %>%
-    select(FSroi, label)
 
-places <- read.table(file_unrotated_coord,
-                     col.names=c("row", "col", "row_o", "col_o", "place"))
+lut_rat <- function(lut="/opt/ni_tools/freesurfer/ASegStatsLUT.txt", scout="FS_warp/*_aseg_scout.nii.gz"){
+    # there are values for each rgba in aseg. will get a warning but dont care
+    # but didnt' do the same for HBT subset. s
+    lut <- read.table(lut)[,1:2] %>% rename(FSroi=V1, label=V2)
 
-tot_vox <- 729 # 3dUndump ... -srad 4
-nzcnt <- system2("/bin/bash",
-                 stdout=T,
-                 args=c("-c",
-                        shQuote("3dROIstats -1DRformat -nomeanout -nzvoxels -mask FS_warp/*_aseg_scout.nii.gz ./placements.nii.gz'<'{1..12}'>'|sed 's/^.*<\\|>_.*?\\]//g;'")))
+    # needed for col_o and row_o which will be required to merge back to spectrum concentrations
+    places <- read.table(file_unrotated_coord,
+                        col.names=c("row", "col", "row_o", "col_o", "place"))
+
+    tot_vox <- 729 # 3dUndump ... -srad 4
+
+    # fancy bash + afni trick:
+    # {1..12} makes 12 input arguments. I
+    # <1> is masked only at nifti values == 1
+    nzcnt <- system2("/bin/bash",
+                    stdout=T,
+                    args=c("-c", shQuote(paste0(
+                    "3dROIstats -1DRformat -nomeanout -nzvoxels -mask ", scout,
+                    " ./placements.nii.gz'<'{1..12}'>'|sed 's/^.*<\\|>_.*?\\]//g;'"))))
 
 
-roi_long <- read.table(text=nzcnt,header=T) %>%
-   rename(place=name) %>%
-   gather("FSroi","rat",-place) %>%
-   mutate(FSroi=gsub("NZcount_","",FSroi) %>% as.numeric) %>%
-   filter(rat!=0)%>%
-   mutate(rat=rat/tot_vox)
+    roi_long <- read.table(text=nzcnt, header=T) %>%
+    rename(place=name) %>%
+    gather("FSroi", "rat", -place) %>%
+    mutate(FSroi=gsub("NZcount_", "", FSroi) %>% as.numeric) %>%
+    filter(rat != 0) %>%
+    mutate(rat=rat/tot_vox)
 
-roi_long_unassigned <- rbind(roi_long,
-                             roi_long %>% group_by(place) %>% summarise(FSroi=0, rat=1-sum(rat)))
+    roi_long_unassigned <- rbind(roi_long,
+                                roi_long %>% group_by(place) %>% summarise(FSroi=0, rat=1-sum(rat)))
 
-FS_roi <- merge(roi_long_unassigned, lut, by="FSroi", all.x=T) %>%
-    merge(places, all=T) %>%
-   arrange(place)
+    FS_roi <- merge(roi_long_unassigned, lut, by="FSroi", all.x=T) %>%
+        merge(places, all=T) %>%
+    arrange(place)
+}
 
+FS_roi <- lut_rat()
 write.csv(file="hc_aseg_roirat.csv", FS_roi, row.names=F, quote=F)
-
 FS_roi %>% group_by(place) %>% filter(rat==max(rat)) %>%
     write.csv(file="hc_aseg_roirat_highest.csv", row.names=F, quote=F)
+
+HBT_roi <- lut_rat("/Volumes/Hera/Projects/7TBrainMech/scripts/mri/Hc/txt/hc_fs_lut.txt",
+                  "FS_warp/*_HBTlr500_scout.nii.gz")
+write.csv(file="hc_HBT_roirat.csv", HBT_roi, row.names=F, quote=F)
+HBT_roi %>% group_by(place) %>% filter(rat==max(rat)) %>%
+    write.csv(file="hc_HBT_roirat_highest.csv", row.names=F, quote=F)
