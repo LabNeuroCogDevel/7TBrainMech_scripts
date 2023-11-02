ed6e130089b12cc2b9802701b058e01eac6998db
WillForan <willforan@gmail.com>
2023-11-02 10:14:42 -0400

    🐛 clean_eog_visit.R: sep trial and visit outlier values. disable visit outlier

--- eeg/eog_cal/clean_eog_visit.R
+++ eeg/eog_cal/clean_eog_visit.R
@@ -3,18 +3,20 @@ library(dplyr); library(tidyr)
 
 # 20230616WF - init, from SM
 #  - add n and ndrop
-#  
+# 20231102WF - separate zthresh for trials and visits.
+#  extra columns for yes/no zthres visit
 
-z_thresh <- 2
+trial_z_thresh <- 2  # exclude bad data/bad model fits
+visit_z_thresh <- 2  # visits mean/sd that are bad. 20231102: cols w/ and w/o
 deg_thresh <- 23
 lat_thresh <- 0.1 # 100ms. is 60ms elsewhere
 in_file <- 'eye_scored_mgs_eog.csv'
 out_file <- 'eye_scored_mgs_eog_cleanvisit.csv'
 
-znathres <- function(x) ifelse(abs(scale(x))[,1]>=z_thresh,NA,x)
+znathres <- function(x, z_thresh) ifelse(abs(scale(x))[,1]>=z_thresh,NA,x)
 test_thres <- function(){
    x <- c(10000,1,1,0,0,0,0)
-   testthat::expect_equal(c(NA,x[2:7]), znathres(x))
+   testthat::expect_equal(c(NA,x[2:7]), znathres(x,2))
 }
 
 # LunaID,ScanDate,Trial,Delay,DelaySaccades,PositionError,DisplacementError,BestError,vgsLatency,mgsLatency,calR2
@@ -25,9 +27,9 @@ eog_clean <- eog %>%
           across(c("PositionError","BestError"), abs), # keep side information
           across(matches("Error"),         function(x) ifelse(abs(x)>deg_thresh,NA,x)),
           across(matches("Latency"),       function(x) ifelse(x     <lat_thresh,NA,x)),
-          across(matches("Latency|Error"), znathres))
+          across(matches("Latency|Error"), \(x) znathres(x,trial_z_thresh)))
 
-# collapse across all trial 
+# collapse across all trial
 err_lat_collapse <- function(eog_clean)
    eog_clean %>%
    group_by(LunaID,ScanDate,Delay) %>%
@@ -37,10 +39,17 @@ err_lat_collapse <- function(eog_clean)
                          ndrop=function(x) length(which(is.na(x))))),
              n=n()) %>%
    ungroup() %>%
-   mutate(across(matches("(Error|Latency)_(mean|sd)"), znathres)) %>%
+   mutate(across(matches("(Error|Latency)_(mean|sd)"),
+                 list("zthres2"=\(x) znathres(x,visit_z_thresh),
+                      "noexclude"=\(x) znathres(x,Inf)))) %>%
+   # keep sd2 names as the default. to overwrite: remove and then rename
+   select(-matches("(Error|Latency)_(sd|mean)$")) %>%
+   rename_with(\(x) gsub('_zthres2','',x)) %>%
+
    # make columns like 'BestError_Delay6'
    pivot_wider(id_cols=c("LunaID","ScanDate"), names_from="Delay", values_from=matches("Error|Latency|^n$")) %>%
    rename_all(function(x) gsub('_mean','',x))
+
 
 # want a collapsed across delay ('*_DelayAll' columns) and each delay enumerated version ('*Delay6',...'*Delay10')
 eog_visit_nodly <- eog_clean %>% mutate(Delay='DelayAll') %>% err_lat_collapse
