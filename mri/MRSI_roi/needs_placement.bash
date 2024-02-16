e3fa4ec256880f568af8212aeb7b1a401154f01d
WillForan <willforan@gmail.com>
2024-02-16 14:26:05 -0500

    ✨ MRSI:needs_placmennt using known_bad_visits

--- mri/MRSI_roi/needs_placement.bash
+++ mri/MRSI_roi/needs_placement.bash
@@ -10,5 +10,9 @@ trap 'e=$?; [ $e -ne 0 ] && echo "$0 exited in error"' EXIT
 #     needs_placement shows where '13MP20200207' does not exist
 cd $(dirname $0)
 for d in ../../../subjs/1*_2*/slice_PFC/; do
-   [ ! -r $d/MRSI_roi/13MP20200207 ] && ld8 $d || :;
+   ld8=$(ld8 "$d")
+   [ -z "$ld8" ] && continue
+   grep "^$ld8.*TODO" known_bad_visit.txt && continue
+   grep -q "^$ld8" known_bad_visit.txt && continue
+   [ ! -r "$d/MRSI_roi/13MP20200207" ] && echo "$ld8" || :;
 done
