1eb878624a2846489292a70c5392e168bc86e489
WillForan <willforan@gmail.com>
2024-06-14 14:54:13 -0400

    ✨ 02_roimean_hurst.bash: add glass, refactor nroi

--- mri/hurst/02_roimean_hurst.bash
+++ mri/hurst/02_roimean_hurst.bash
@@ -10,7 +10,7 @@ USAGE:
    $0 [-h] -indir ts/atals-x_prefix-y -atlas altas/x.nii.gz -method [dfa|hurst_rs]
 
    -indir    folder with 3dROIstats output, likely from 01_roi-meants.bash
-   -atlas    ROI atlas
+   -atlas    ROI atlas (ROI labels looked up by atlas name)
    -method   must be either dfa (likely what you want) or hurst_rs
    -h        this help
 
@@ -46,6 +46,9 @@ parse_hurst_args(){
    [[ -z "$ATLAS" || -z "$INDIR" || -z "$METHOD" || -z "$OUTPUT" ]] &&
       echo "-atlas, -method, -output and -indir are all required!" && exit 1
    ! [[ $METHOD =~ ^(hurst_rs|dfa)$ ]] && echo "method '$METHOD' must be hurst_rs or dfa" && exit 1
+
+   # TODO: this is unnessary?
+   #  we dont need the file to exist and we'll bail elsewhere if we dont understand the atlas name
    [[ ! -r "$ATLAS" && ! $ATLAS =~ mrsi13 ]] && echo "Cannot read atlas file '$ATLAS'" && exit 2
    return 0
 }
@@ -55,24 +58,35 @@ calc_hurst(){
     local saveas="${2:?output name}"
     local method="${3:?dfa or hurst_rs}"
     local atlas="${4:?roi atlas nifti file}"
+    local nroi lut
     case $atlas in
        *mancov*) roi_labels=(4_LPostInsula 5_RCaudate 7_ACC 8_MPFC 9_RDLPFC 10_LDLPFC);;
        #{subject_glob}/mrsipfc13_nzmean{in_prefix}_ts.1D
        *13MP*|*pfc13*|*mrsi13*)
+                  nroi=13
                   roi_labels=(1_RAntInsula 2_LAntInsula 3_RPostInsula 4_LPostInsula 5_RCaudate 6_LCaudate 7_ACC 8_MPFC 9_RDLPFC 10_LDLPFC 11_RSTS 12_LSTS 13_RThal);;
        *mni_gm50_mask.nii.gz|*onlyGM*)
+                 nroi=1
                  roi_labels=(all_gm);;
        *schaefer*200*)
+                 nroi=200
                  mapfile -t roi_labels < <(seq 1 200);;
        *schaefer*100*)
           ! [[ $atlas =~ schaefer([0-9]*)_  ]] && echo "could not determin number of rois in '$atlas'" && exit 1
-          local nroi=${BASH_REMATCH[1]}
-          local lut="$SCHAEFER_LUT_DIR/Schaefer2018_${nroi}Parcels_17Networks_order.lut"
+          nroi=${BASH_REMATCH[1]}
+          lut="$SCHAEFER_LUT_DIR/Schaefer2018_${nroi}Parcels_17Networks_order.lut"
           [ ! -r "$lut" ] && echo "bad lookup table file '$lut'" && exit 1
-          mapfile -t roi_labels < <(cut -d' ' -f5 "$lut")
-          ! [[ "${#roi_labels[@]}" -eq $nroi ]] && echo "ERROR: bad n ROIS (${#roi_labels[@]}!=$nroi) in '$lut'" && exit 1;;
-       *) echo "unknown atlas '$atlas', want to match mancov, 13MP or mni_gm"; exit 3;;
+          mapfile -t roi_labels < <(cut -d' ' -f5 "$lut");;
+      *glass*)
+          nroi=360
+          local lut="/opt/ni_tools/atlas/MMP_1.0_MNI-Glasser2016/Glasser_2016_Table.csv"
+          [ ! -r "$lut" ] && echo "bad lookup table file '$lut'" && exit 1
+          mapfile -t roi_labels < <(cut -d',' -f2-3 "$lut"|sed '1d;s/,/_/g');;
+
+       *) echo "unknown atlas '$atlas', want to match mancov, 13MP, mni_gm, schaefer, glasser"; exit 3;;
     esac
+
+    ! [[ "${#roi_labels[@]}" -eq $nroi ]] && echo "ERROR: bad n ROIS (${#roi_labels[@]}!=$nroi) in '$lut'" && exit 1
     # newer ts calc in local ts/ . native space mrsi13 is within preproc dir
     [ -d "$ts_folder" ] && glob="$ts_folder/1*_2*.1D" || glob="$ts_folder"
     ./hurst_1d.py  --input "$glob" --output "$saveas" --method "$method" --roilabels "${roi_labels[@]}"
