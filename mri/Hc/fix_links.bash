6caa536529bef94a5ad342335eab71796c5280b7
WillForan <willforan@gmail.com>
2025-06-26 10:07:43 -0400

    fix_links.bash: also fix siarray.1.1

--- mri/Hc/fix_links.bash
+++ mri/Hc/fix_links.bash
@@ -5,10 +5,12 @@
 # 20250109WF - init
 #
 #
+i=0
 
-find spectrum/2*/anat.mat \
-   -xtype l  -iname 'anat.mat' -printf "%p %l\n"|
+find spectrum/2*/{siarray.1.1,anat.mat} \
+   -xtype l  \( -iname 'anat.mat' -or -iname 'siarray.1.1' \)  -printf "%p %l\n"|
  while read -r p l; do
+  ((++i))
   new=$(sed -E 's:/7TBrainMech/2[0-9]+/:/7TBrainMech/duplicate-folders-collected-20240328/:' <<< "${l}")
   # /Volumes/Hera/Raw/MRprojects/7TBrainMech/duplicate-folders-collected-20240328/20180913Luna1-fromHCCollection
   if ! [ -r "$new" ]; then
@@ -19,9 +21,11 @@ find spectrum/2*/anat.mat \
      patt=$(sed -E 's:7TBrainMech/2[0-9]+/(2[0-9]+[^/]+):7TBrainMech/duplicate-folders-collected-20240328/\1-from*:' <<< "${l}")
      new=$(ls $patt 2>/dev/null | sed 1q || : )
   fi
+
+  [ -n "${VERBOSE:-}" ] && echo "# $p $l '$new' # $i $(date)"
   if ! [ -r "$new" ]; then
      warn "# missing '$p', '$patt' does not exist. original '$l'" 
      continue
   fi
   dryrun ln -sf "$new" "$p"
-done
+done | tee -a txt/fixlinks.log
